1f7323a2f30c3d059deb22d89470877371ce095f
larmic <mail@larmic.de>
2015-11-24 12:12:47 +0100

    add list of 399 changeable items

---
+++ src/main/java/de/larmic/butterfaces/performance/IndexBean.java
@@ -0,0 +1,27 @@
+package de.larmic.butterfaces.performance;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+@SessionScoped
+@Named
+public class IndexBean implements Serializable {
+
+    private final List<ListItem> items = new ArrayList<>();
+    
+    @PostConstruct
+    public void init() {
+        IntStream.range(0, 399).forEach(
+                index -> items.add(new ListItem("name" + index, "description" + index))
+        );
+    }
+
+    public List<ListItem> getItems() {
+        return items;
+    }
+}
